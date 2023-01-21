45600a183644d8dbed3b843a3e1ca9ec37f62d0f
Py Meadow <pymeadow@gmail.com>
2023-01-21 16:39:20 -0500

    categorized parse failures into 3 types

--- datasets/corpus_fail/eval_failures_1.bash
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-PYTHONPATH=. python tests/pipe_entail.py --input_file datasets/corpus_fail/failures_1.txt
---
+++ datasets/corpus_fail/eval_sem_fail.bash
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+PYTHONPATH=. python tests/pipe_entail.py --input_file datasets/corpus_fail/sem_fail.txt
